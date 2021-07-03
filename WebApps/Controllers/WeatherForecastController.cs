f62d47cb1a81bd8e1c0e197c1af8d00c1cd6165e
Talha <syedtalha30@gmail.com>
2021-07-03 22:54:22 +0500

    Add some data to check the code

--- WebApps/Controllers/WeatherForecastController.cs
+++ WebApps/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApps.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "LPC TALHA", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
